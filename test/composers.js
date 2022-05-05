348c4768cff83bbe5b501547218e9183e89dd1c2
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-05-05 12:26:47 -0700

    added composers array

---
+++ test/composers.js
@@ -0,0 +1,7 @@
+const composers = [{ name: 'J.S Bach', era: 'Baroque', hits: 
+[{ pieceName: 'Cello Suite in G Major', Classification: 'BWV 1007' }, { pieceName: 'Brandenburg Concerto No. 3', Classification: 'BWV 1048' }, { pieceName: 'Brandenburg Concerto No. 3', Classification: 'BWV 1048' }]
+}, { name: 'Beethoven', era: 'Classical', hits: 
+[{ pieceName: '5th Symphony', Classification: 'Op. 67' }, { pieceName: 'Moonlight Sonata', Classification: 'Op. 27 No. 2' }, { pieceName: '9th Symphony', Classification: 'Op. 125' }]
+}, { name: 'Beethoven', era: 'Classical', hits: 
+[{ pieceName: '5th Symphony', Classification: 'Op. 67' }, { pieceName: 'Moonlight Sonata', Classification: 'Op. 27 No. 2' }, { pieceName: '9th Symphony', Classification: 'Op. 125' }]
+}];
